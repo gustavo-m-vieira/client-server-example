92fc7bf2ae624b9a18c087b0b5cd91178d2adc42
Gustavo <gustavo@bgcbrasil.com.br>
2021-07-17 23:08:06 -0300

    class 22

--- packages/web/src/plugins/apollo/link.js
+++ packages/web/src/plugins/apollo/link.js
@@ -1,9 +1,21 @@
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, Observable } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { onError } from 'apollo-link-error';
 
+const loggerLink = new ApolloLink((operation, forward) => new Observable(observer => {
+  forward(operation).subscribe({
+    complete: observer.complete.bind(observer),
+    error: observer.complete.bind(observer),
+    next: result => {
+      console.log('Log', result);
+      observer.next(result);
+    }
+  });
+}));
+
 const link = ApolloLink.from([
+  loggerLink,
   onError((error) => {
     console.error('GraphQLError', error);
   }),
